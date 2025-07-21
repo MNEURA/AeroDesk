edfa48528425e30958edf57987bcfc6d331a6837
MNEURA <rpmcrbm@gmail.com>
2025-07-21 13:32:20 -0700

    MySQL.java was created

---
+++ src/utill/MySQL.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utill;
+
+/**
+ *
+ * @author mneura
+ */
+public class MySQL {
+    public static void main(String[] args) {
+        
+    }
+}
